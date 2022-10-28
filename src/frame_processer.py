755973f130358845f9383bfa431597a9398fc4be
Ask Røsand <askbr@stud.ntnu.no>
2022-10-28 19:47:36 +0200

    Implemented frame pre processor as a class

--- src/frame_processer.py
+++ src/frame_processer.py
@@ -1,19 +1,33 @@
 import cv2
+import numpy as np
 
 
-def img_to_gray(img):
-    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+class ImagePreProcessor:
+    def __init__(self, initial_height, initial_width, crop_pixels_top, crop_pixels_bottom, do_gray, do_crop):
+        self.initial_height = initial_height
+        self.initial_width = initial_width
 
+        self.crop_pixels_top = crop_pixels_top
+        self.crop_pixels_bottom = crop_pixels_bottom
 
-def img_crop_height(img, pixels_from_top, pixels_from_bottom):
-    return img[pixels_from_top:img.shape[0] - pixels_from_bottom, :]
+        self.processed_height = initial_height - crop_pixels_top - crop_pixels_bottom
+        self.processed_width = initial_width
 
+        self.do_gray = do_gray
+        self.do_crop = do_crop
 
-def get_processed_img(img, to_gray=True, pixels_from_top=25, pixels_from_bottom=5):
-    if to_gray:
-        img = img_to_gray(img)
+    def get_processed_img(self, img):
+        if self.do_gray:
+            img = self.__img_to_gray(img)
 
-    img = img_crop_height(img, pixels_from_top, pixels_from_bottom)
-    return img
+        img = self.__img_crop_height(img)
+        return img
 
+    def get_black_image(self):
+        return np.zeros((self.processed_height, self.processed_width))
 
+    def __img_crop_height(self, img):
+        return img[self.crop_pixels_top:(img.shape[0] - self.crop_pixels_bottom), :]
+
+    def __img_to_gray(self, img):
+        return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
--- test/preprocessing/frame_processer_test.py
+++ test/preprocessing/frame_processer_test.py
@@ -1,5 +1,12 @@
-from src.frame_processer import get_processed_img
+from src.frame_processer import ImagePreProcessor
 import cv2
 
 img = cv2.imread("game_img.png")
-cv2.imwrite("result.png", get_processed_img(img))
+print(type(img))
+p = ImagePreProcessor(img.shape[0], img.shape[1], 25, 5, True, True)
+
+processed = p.get_processed_img(img)
+black = p.get_black_image()
+
+#  cv2.imwrite("processed.png", p.get_processed_img(img))
+#  cv2.imwrite("black.png", p.get_black_image())
