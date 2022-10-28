954786f3fa43beeb878cb6b37d917b8ffa059e80
Ask Røsand <askbr@stud.ntnu.no>
2022-10-28 14:11:56 +0200

    Initial neural network

---
+++ src/networks/dqn.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+# TODO: Make a better model with CNN
+class DQN(nn.Module):
+    def __init__(self, image_height, image_width):
+        super().__init__()
+
+        self.fc1 = nn.Linear(in_features=image_width * image_height * 3, out_features=24)  # *3 because rgb
+        self.fc2 = nn.Linear(in_features=24, out_features=32)
+        self.out = nn.Linear(in_features=32, out_features=6)  # Final layer, 6 actions therefor 6 output nodes
+
+    def forward(self, x):
+        """
+        Attributes:
+            x: an image
+        """
+        x = x.flatten(start_dim=1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.out(x)
+        return x
