da68575db6256277e00041f735beb93c7fd5b7b9
Ask Røsand <askbr@stud.ntnu.no>
2022-11-14 17:48:00 +0100

    Added more options for execution

--- src/algorithms/dqn.py
+++ src/algorithms/dqn.py
@@ -6,6 +6,7 @@ from src.utils.replay_memory import ReplayMemory
 from src.utils.agent import Agent
 from src.networks.nature_cnn import NatureCNN
 from src.networks.ann import ANN
+from src.networks.dueling_cnn import DuelingCNN
 import json
 import time
 
@@ -61,7 +62,7 @@ def get_loss_dqn(batch, policy_net, target_net, gamma, device, loss_function):
 
 def training_loop(env_name, device, memory_size, epsilon_initial, learning_rate, max_frames, memory_size_min, batch_size,
                   gamma, target_net_update, epsilon_min, epsilon_decay, logging_rate, save_to_file, model_path,
-                  params_path, optimizer, loss_function, network):
+                  params_path, optimizer, loss_function, target_q_equation, network):
     # Logging
     frames = []  # Frames where episodes ended
     rewards = []  # Corresponding rewards for the episode
@@ -74,9 +75,15 @@ def training_loop(env_name, device, memory_size, epsilon_initial, learning_rate,
     if network == "ann":
         policy_net = ANN(num_actions).to(device)
         target_net = ANN(num_actions).to(device)
+        print("[INIT] ANN initialized.")
+    elif network == "dueling":
+        policy_net = DuelingCNN(num_actions).to(device)
+        target_net = DuelingCNN(num_actions).to(device)
+        print("[INIT] DuelingCNN initialized.")
     else:
         policy_net = NatureCNN(num_actions).to(device)
         target_net = NatureCNN(num_actions).to(device)
+        print("[INIT] NatureCNN initialized.")
 
     replay_memory = ReplayMemory(memory_size)
     agent = Agent(env_manager, replay_memory, device)
@@ -85,8 +92,10 @@ def training_loop(env_name, device, memory_size, epsilon_initial, learning_rate,
     # Optimizer
     if optimizer == "rmsprop":
         opt = optim.RMSprop(policy_net.parameters(), lr=learning_rate)
+        print("[INIT] Optimizer RMSprop initialized.")
     else:
         opt = optim.Adam(policy_net.parameters(), lr=learning_rate)
+        print("[INIT] Optimizer Adam initialized.")
 
     # Training loop
     time_start = time.time()
@@ -101,7 +110,12 @@ def training_loop(env_name, device, memory_size, epsilon_initial, learning_rate,
 
         if len(replay_memory) >= memory_size_min:  # Perform training
             batch = replay_memory.get_sample(batch_size)
-            loss = get_loss_dqn(batch, policy_net, target_net, gamma, device, loss_function)
+
+            if target_q_equation == "ddqn":
+                loss = get_loss_ddqn(batch, policy_net, target_net, gamma, device, loss_function)
+            else:
+                loss = get_loss_dqn(batch, policy_net, target_net, gamma, device, loss_function)
+
             loss.backward()
             opt.step()
             opt.zero_grad()
@@ -141,4 +155,4 @@ def train_from_settings(settings_path):
     training_loop(s["env_name"], device, s["memory_size"], s["epsilon_initial"], s["learning_rate"], s["max_frames"],
                   s["memory_size_min"], s["batch_size"], s["gamma"], s["target_net_update"], s["epsilon_min"],
                   s["epsilon_decay"], s["logging_rate"], s["save_to_file"], s["model_path"], s["params_path"],
-                  s["optimizer"], s["loss_function"], s["network"])
+                  s["optimizer"], s["loss_function"], s["target_q_equation"], s["network"])
